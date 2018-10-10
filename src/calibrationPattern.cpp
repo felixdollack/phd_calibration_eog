9fc8fe55bd141e933439f21774e49c2fe24bcaa8
Felix Dollack <felix.dollack@googlemail.com>
2018-10-10 10:16:18 +0900

    Add minimal calibration pattern class
    This commit adds an empty class for calibration patterns.

---
+++ src/calibrationPattern.cpp
@@ -0,0 +1,11 @@
+//
+//  calibrationPattern.cpp
+//  phd_calibration_eog
+//
+//  Created by Felix Dollack on 10.10.18.
+//
+
+#include "calibrationPattern.h"
+
+CalibrationPattern::CalibrationPattern() {
+}
---
+++ src/calibrationPattern.h
@@ -0,0 +1,18 @@
+//
+//  calibrationPattern.h
+//  phd_calibration_eog
+//
+//  Created by Felix Dollack on 10.10.18.
+//
+
+#ifndef calibrationPattern_h
+#define calibrationPattern_h
+
+#include "ofMain.h"
+
+class CalibrationPattern {
+public:
+    CalibrationPattern();
+};
+
+#endif /* calibrationPattern_h */
