1fc89c1c44f028cc78721b77f61adafbb02722e3
Felix Dollack <felix.dollack@googlemail.com>
2018-10-10 15:32:14 +0900

    Refactor settings functions for readability
    This commit refactors load and write default settings functions.

--- src/calibrationPattern.cpp
+++ src/calibrationPattern.cpp
@@ -25,56 +25,73 @@ void CalibrationPattern::resizePattern(float window_width, float window_height) 
 
 void CalibrationPattern::loadSettings() {
     this->_pattern_settings->pushTag("calibration");
-    this->_pattern_settings->pushTag("target");
-    this->_marker_radius = this->_pattern_settings->getValue("radius", 2.0f);
+    {
+        this->_pattern_settings->pushTag("target");
+        {
+            this->_marker_radius = this->_pattern_settings->getValue("radius", 2.0f);
 
-    float r,g,b;
-    this->_pattern_settings->pushTag("color");
-    r = this->_pattern_settings->getValue("red",   0);
-    g = this->_pattern_settings->getValue("green", 0);
-    b = this->_pattern_settings->getValue("blue",  0);
-    this->_marker_color = ofColor(r, g, b);
-    this->_pattern_settings->popTag();
+            float r,g,b;
+            this->_pattern_settings->pushTag("color");
+            {
+                r = this->_pattern_settings->getValue("red",   0);
+                g = this->_pattern_settings->getValue("green", 0);
+                b = this->_pattern_settings->getValue("blue",  0);
+                this->_marker_color = ofColor(r, g, b);
+            }
+            this->_pattern_settings->popTag();
+        }
+        this->_pattern_settings->popTag();
 
-    this->_pattern_settings->popTag();
-
-    this->_pattern_settings->pushTag("order");
-    int positions_in_pattern = this->_pattern_settings->getValue("size", 0);
-    for (int i = 0; i < positions_in_pattern; i++) {
-        this->_pattern_settings->pushTag("item", i);
-        this->_target_order.push_back(this->_pattern_settings->getValue("n",  0));
+        this->_pattern_settings->pushTag("order");
+        {
+            int positions_in_pattern = this->_pattern_settings->getValue("size", 0);
+            for (int i = 0; i < positions_in_pattern; i++) {
+                this->_pattern_settings->pushTag("item", i);
+                {
+                    this->_target_order.push_back(this->_pattern_settings->getValue("n",  0));
+                }
+                this->_pattern_settings->popTag();
+            }
+        }
         this->_pattern_settings->popTag();
     }
-    this->_pattern_settings->popTag();
     this->_pattern_settings->popTag();
 }
 
 void CalibrationPattern::writeDefaultSettings() {
     this->_pattern_settings->addTag("calibration");
     this->_pattern_settings->pushTag("calibration");
+    {
+        this->_pattern_settings->addTag("target");
+        this->_pattern_settings->pushTag("target");
+        {
+            this->_pattern_settings->addValue("radius", 12.0f);
+            this->_pattern_settings->addTag("color");
+            this->_pattern_settings->pushTag("color");
+            {
+                this->_pattern_settings->addValue("red", 128);
+                this->_pattern_settings->addValue("green", 0);
+                this->_pattern_settings->addValue("blue", 128);
+            }
+            this->_pattern_settings->popTag();
+        }
+        this->_pattern_settings->popTag();
 
-    this->_pattern_settings->addTag("target");
-    this->_pattern_settings->pushTag("target");
-    this->_pattern_settings->addValue("radius", 12.0f);
-    this->_pattern_settings->addTag("color");
-    this->_pattern_settings->pushTag("color");
-    this->_pattern_settings->addValue("red", 128);
-    this->_pattern_settings->addValue("green", 0);
-    this->_pattern_settings->addValue("blue", 128);
-    this->_pattern_settings->popTag();
-    this->_pattern_settings->popTag();
-
-    this->_pattern_settings->addTag("order");
-    this->_pattern_settings->pushTag("order");
-    this->_pattern_settings->addValue("size", 13);
-    for (int i = 0; i < 13; i++) {
-        this->_pattern_settings->addTag("item");
-        this->_pattern_settings->pushTag("item", i);
-        this->_pattern_settings->addValue("n",  i);
+        this->_pattern_settings->addTag("order");
+        this->_pattern_settings->pushTag("order");
+        {
+            this->_pattern_settings->addValue("size", 13);
+            for (int i = 0; i < 13; i++) {
+                this->_pattern_settings->addTag("item");
+                this->_pattern_settings->pushTag("item", i);
+                {
+                    this->_pattern_settings->addValue("n",  i);
+                }
+                this->_pattern_settings->popTag();
+            }
+        }
         this->_pattern_settings->popTag();
     }
-    this->_pattern_settings->popTag();
-
     this->_pattern_settings->popTag();
     this->_pattern_settings->saveFile(this->_pattern_settings_filename);
 }
